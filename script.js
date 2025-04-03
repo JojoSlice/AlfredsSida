998ca500994ef4121262a561d04fc0fc3dd50c62
JojoSlice <johannes152@hotmail.com>
2025-04-03 23:17:51 +0200

    Quick Update

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var bilder = document.querySelectorAll("#gallery img");
+  fetch("http://localhost:3000/bilder")
+    .then((response) => response.json())
+    .then((bilder) => {
+      const containerOne = document.getElementById("bildbox1");
+      const containerTwo = document.getElementById("bildbox2");
+      const containerThree = document.getElementById("bildbox3");
 
-  bilder.forEach(function (bild) {
-    bild.addEventListener("click", function () {
-      onToggleView(this);
-    });
-  });
+      bilder.forEach((bild, index) => {
+        const bildBox = document.createElement("div");
+        bildBox.classList.add("p-2");
+
+        const img = document.createElement("img");
+        img.src = `http://localhost:3000/${bild}`;
+        img.alt = "bild";
+
+        bildBox.appendChild(img);
+
+        img.addEventListener("click", function () {
+          onToggleView(this);
+        });
+
+        if (index % 3 === 0) {
+          containerOne.appendChild(bildBox);
+        } else if (index % 3 === 1) {
+          containerTwo.appendChild(bildBox);
+        } else {
+          containerThree.appendChild(bildBox);
+        }
+      });
+    })
+    .catch((error) => console.error("Fel vid hämtning:", error));
 });
+
 function onToggleView(bild) {
   var storBild = document.getElementById("imageview");
   if (storBild.src === bild.src) {
@@ -19,12 +44,14 @@ function onToggleView(bild) {
     view.style.display = "flex";
   }
 }
+
 function closeView(e) {
   if (view.style.display != "none") {
     view.style.display = "none";
     e.src = "hide.jpg";
   }
 }
+
 const navLinks = document.querySelector(".nav-links");
 function onToggleMenu(e) {
   e.src = e.src.includes("menu.svg") ? "images/close.svg" : "images/menu.svg";
